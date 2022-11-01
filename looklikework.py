fc14dde4f573caffdbf576948ef3334e8f006db2
Zigmata <scafidif@gmail.com>
2022-11-01 15:23:35 -0700

    PEP8 formatting unfucked. Get a real IDE

--- looklikework.py
+++ looklikework.py
@@ -3,8 +3,10 @@ from time import sleep
 from tqdm import tqdm
 from random import *
 import random
-#Colors
-class bcolors:
+
+
+# Colors
+class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[90m'
@@ -15,31 +17,31 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-#print(f'Successfully downloaded {bcolors.OKGREEN}hello {bcolors.ENDC} to {format}')
+# print(f'Successfully downloaded {BColors.OKGREEN}hello {BColors.ENDC} to {format}')
 os.system('clear')
 
 things = [
-"productize web-enabled web services",
-"repurpose ubiquitous users",
-"e-enable value-added dark web",
-"aggregate sticky users",
-"repurpose bleeding-edge SaaS"
+    "productize web-enabled web services",
+    "repurpose ubiquitous users",
+    "e-enable value-added dark web",
+    "aggregate sticky users",
+    "repurpose bleeding-edge SaaS"
 ]
 verbs = ['aggregate', 'benchmark', 'deploy', 'extend', 'implement']
-nouns = ['Database','Container', 'Virtual Machine', 'Data', 'Network']
+nouns = ['Database', 'Container', 'Virtual Machine', 'Data', 'Network']
 
 for t in things:
-    
-    print(f'{bcolors.OKBLUE}{t} {bcolors.ENDC}')
+
+    print(f'{BColors.OKBLUE}{t} {BColors.ENDC}')
     x = 0
-    while(x<4):
+    while x < 4:
         x = x + 1
         verb = random.choice(verbs)
         noun = random.choice(nouns)
         print(f'{verb} {noun} ...')
-        for i in tqdm(range(randint(1,20))):
+        for i in tqdm(range(randint(1, 20))):
             sleep(1)
         sleep(5)
         passfail = ['pass', 'fail']
 
-        print(f'{bcolors.WARNING} {random.choice(passfail)}{bcolors.ENDC} ')
+        print(f'{BColors.WARNING} {random.choice(passfail)}{BColors.ENDC} ')
