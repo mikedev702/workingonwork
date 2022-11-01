fcefde917700b7e6ed18780c9154458c20a1e548
Michael Hall <michael@michaelhall.rocks>
2022-11-01 21:34:00 +0000

    init

---
+++ looklikework.py
@@ -0,0 +1,37 @@
+import os
+from time import sleep
+from tqdm import tqdm
+from random import *
+#Colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[90m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+#print(f'Successfully downloaded {bcolors.OKGREEN}hello {bcolors.ENDC} to {format}')
+os.system('clear')
+
+things = [
+"productize web-enabled web services",
+"repurpose ubiquitous users",
+"e-enable value-added dark web",
+"aggregate sticky users",
+"repurpose bleeding-edge SaaS"
+        ]
+
+for t in things:
+    
+    print(f'{bcolors.OKBLUE}{t} {bcolors.ENDC}')
+    x = 0
+    while(x<10):
+        x = x + 1
+        print('Setup...')
+        for i in tqdm(range(randint(1,20))):
+            sleep(3)
+        sleep(5)
