46a128f72af5ae2952456e9aa9229f85e5fe2a65
Zigmata <scafidif@gmail.com>
2022-11-01 15:18:26 -0700

    PEP8 formatting unfucked. Get a real IDE

--- looklikework.py
+++ looklikework.py
@@ -2,8 +2,10 @@ import os
 from time import sleep
 from tqdm import tqdm
 from random import *
-#Colors
-class bcolors:
+
+
+# Colors
+class BColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[90m'
@@ -14,24 +16,24 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-#print(f'Successfully downloaded {bcolors.OKGREEN}hello {bcolors.ENDC} to {format}')
+# Print(f'Successfully downloaded {bcolors.OKGREEN}hello {bcolors.ENDC} to {format}')
 os.system('clear')
 
 things = [
-"productize web-enabled web services",
-"repurpose ubiquitous users",
-"e-enable value-added dark web",
-"aggregate sticky users",
-"repurpose bleeding-edge SaaS"
-        ]
+    "productize web-enabled web services",
+    "repurpose ubiquitous users",
+    "e-enable value-added dark web",
+    "aggregate sticky users",
+    "repurpose bleeding-edge SaaS"
+]
 
 for t in things:
-    
-    print(f'{bcolors.OKBLUE}{t} {bcolors.ENDC}')
+
+    print(f'{BColors.OKBLUE}{t} {BColors.ENDC}')
     x = 0
-    while(x<10):
+    while x < 10:
         x = x + 1
         print('Setup...')
-        for i in tqdm(range(randint(1,20))):
+        for i in tqdm(range(randint(1, 20))):
             sleep(3)
         sleep(5)
